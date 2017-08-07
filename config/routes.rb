836c6875fb132a4d88293ed34b1916b572684c19
David Cuadra <dcuadraq@gmail.com>
2017-08-07 17:10:07 -0500

    Add full list of providers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Spree::Core::Engine.routes.draw do
   namespace :api, defaults: { format: 'json' } do
-    ['facebook'].each do |provider, _value|
+    # TODO; .beautify(this)
+    %w(facebook twitter amazon github google_oauth2).each do |provider, _value|
       post "/#{provider}/signin", to: 'social_auths#signin'
       post "/#{provider}/signup", to: 'social_auths#signup'
     end
--- spec/controllers/spree/api/social_auths_controller_spec.rb
+++ spec/controllers/spree/api/social_auths_controller_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
 
 describe Spree::Api::SocialAuthsController do
-  it 'holi' do
-    expect(post: '/api/facebook/signin').to route_to(controller: 'socials', action: 'signin')
+  it 'offers endpoint for amazon' do
+    expect(post: '/api/amazon/signin').to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'signin')
+  end
+
+  it 'offers endpoint for facebook' do
+    expect(post: '/api/facebook/signin').to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'signin')
+  end
+
+  it 'offers endpoint for github' do
+    expect(post: '/api/github/signin').to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'signin')
+  end
+
+  it 'offers endpoint for google' do
+    expect(post: '/api/google_oauth2/signin').to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'signin')
+  end
+
+  it 'offers endpoint for twitter' do
+    expect(post: '/api/twitter/signin').to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'signin')
   end
 end
