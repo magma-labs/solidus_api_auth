a7618753f365d7619b78237749fc212ad526f93f
David Cuadra <dcuadraq@gmail.com>
2017-08-08 13:31:35 -0500

    Removed hard coded paths on specs

--- spec/controllers/spree/api/social_auths_controller_spec.rb
+++ spec/controllers/spree/api/social_auths_controller_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe Spree::Api::SocialAuthsController do
   it 'offers endpoint for amazon' do
-    expect(post: '/api/amazon/signin').to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'amazon_signin')
+    expect(post: api_amazon_signin_path).to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'amazon_signin')
   end
 
   it 'offers endpoint for facebook' do
-    expect(post: '/api/facebook/signin').to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'facebook_signin')
+    expect(post: api_facebook_signin_path).to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'facebook_signin')
   end
 
   it 'offers endpoint for github' do
-    expect(post: '/api/github/signin').to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'github_signin')
+    expect(post: api_github_signin_path).to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'github_signin')
   end
 
   it 'offers endpoint for google' do
-    expect(post: '/api/google_oauth2/signin').to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'google_oauth2_signin')
+    expect(post: api_google_oauth2_signin_path).to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'google_oauth2_signin')
   end
 
   it 'offers endpoint for twitter' do
-    expect(post: '/api/twitter/signin').to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'twitter_signin')
+    expect(post: api_twitter_signin_path).to route_to(format: 'json', controller: 'spree/api/social_auths', action: 'twitter_signin')
   end
 end
